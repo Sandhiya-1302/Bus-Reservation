acd9d54f131e867ee3089ace8261394ef0bcc2a9
Sandhiya-1302 <144993367+Sandhiya-1302@users.noreply.github.com>
2024-02-05 21:30:32 +0530

    Booking.java

---
+++ Booking.java
@@ -0,0 +1,46 @@
+package Reservation;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class Booking {
+    String passengerName;
+    int busNo;
+    Date date;
+
+    Booking(){
+        try (Scanner s = new Scanner(System.in)) {
+            System.out.println("Enter name of the passenger: ");
+            passengerName=s.next();
+            System.out.println("Enter bus No: ");
+            busNo=s.nextInt();
+            System.out.println("Enter the booking date (dd-mm-yyyy)");
+            String ipDate=s.next();
+            SimpleDateFormat dateFormat=new SimpleDateFormat("dd-mm-yyyy");
+//		s.close();
+
+            try {
+                date=dateFormat.parse(ipDate);
+            }catch(ParseException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    public boolean isAvailable(ArrayList<Bus> buses,ArrayList<Booking> bookings) {
+        int capacity=0,booked=0;
+        for(Bus bus:buses) {
+            if(bus.busNo==busNo) {
+                capacity=bus.getSeats();
+            }
+        }
+        for(Booking booking:bookings) {
+            if(booking.busNo==busNo && booking.date.equals(date)) {
+                booked++;
+            }
+        }
+        return booked<capacity ? true:false;
+    }
+}
